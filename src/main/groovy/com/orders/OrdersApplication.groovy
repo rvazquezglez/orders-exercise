00b5a8e2495c0791b02a02be5d0cab329eb0af14
rvazquezglez <rvazquezglez@gmail.com>
2017-07-08 02:27:50 -0700

    Using sample data from db instead of hardcoded data.

--- src/main/groovy/com/orders/OrdersApplication.groovy
+++ src/main/groovy/com/orders/OrdersApplication.groovy
@@ -1,15 +1,8 @@
 package com.orders
 
-import com.orders.models.Customer
-import com.orders.models.Item
-import com.orders.models.Order
-import com.orders.models.Payment
-import com.orders.repositories.OrderRepository
-import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.domain.EntityScan
-import org.springframework.context.annotation.Bean
 import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters
 
 @EntityScan(
@@ -21,27 +14,5 @@ class OrdersApplication {
 
     static void main(String[] args) {
         SpringApplication.run OrdersApplication, args
-    }
-
-    @SuppressWarnings("GroovyUnusedDeclaration") // Method is used by Spring
-    @Bean
-    CommandLineRunner setup(OrderRepository orderRepository) {
-        { args ->
-            orderRepository.save(new Order(
-                    customer: new Customer(),
-                    itemsPurchased: [
-                            new Item(
-                                    name: "book",
-                                    amount: 40.0
-                            ),
-                            new Item(
-                                    name: "rechargeable batteries",
-                                    amount: 60.0
-                            )
-                    ],
-                    paymentsReceived: [new Payment(amount: 100)],
-                    totalAmount: 100
-            ))
-        }
     }
 }
---
+++ src/main/resources/db/migration/V1.1__inserts.sql
@@ -0,0 +1,4 @@
+INSERT INTO customers(name, last_name) VALUES ('John', 'Lennon');
+INSERT INTO orders (date, total_amount, customer_id) VALUES ('2017-06-06', 100, 1);
+INSERT INTO items (name, amount, order_id) VALUES ('book', 100, 1);
+INSERT INTO payments (date, amount, order_id) VALUES ('2017-06-06', 100, 1);
